2f7ad7b1ce6af4bc755a871523978e9d61e59d53
BAEJUN SON <sonqowns2@naver.com>
2022-10-15 17:32:31 +0900

    Image-Processing second assignment

--- pythonProject/Image-Processing second assignment/practice4.py
+++ pythonProject/Image-Processing second assignment/practice4.py
@@ -1,40 +1,30 @@
 import numpy as np, cv2
 
-def bar(value) :
-    global one, two, title, image1, image2, dst
+image1 = cv2.imread("images/add1.jpg", cv2.IMREAD_GRAYSCALE)                # GRAYSCALE로 영상읽기
+image2 = cv2.imread("images/add2.jpg", cv2.IMREAD_GRAYSCALE)                # GRAYSCALE로 영상읽기
+if image1 is None or image2 is None: raise Exception("영상 파일 읽기 오류 발생") # 예외 처리
 
-    # one, bete값 구하기
-    one = cv2.getTrackbarPos('image1', title) / 100
-    two = cv2.getTrackbarPos('image2', title) / 100
-
-    # one, two값으로 영상 합성하기
-    image3 = cv2.addWeighted(image1, one, image2, two, 0)  # 두영상 비율에 따른 더하기
-    dst[0:h, w:w*2] = image3[0:h, 0:w]                        # 배열의 가운데에 image3 넣기
-
-    cv2.imshow(title, dst)  #윈도우에 영상 표시
+proportion = [50, 50]                                                       # 초기값 설정
+title = "dst"                                                               # title 선언
 
 
-# 영상 읽기
-image1 = cv2.imread("images/add1.jpg", cv2.IMREAD_GRAYSCALE)
-image2 = cv2.imread("images/add2.jpg", cv2.IMREAD_GRAYSCALE)
-if image1 is None or image2 is None: raise Exception("영상 파일 읽기 오류 발생")
+def desolve():
+    synthesis = cv2.addWeighted(image1, proportion[0], image2, proportion[1], 0)    # image1, image2 addWeight 실행
+    concated = cv2.hconcat([image1, synthesis, image2])                             # image1, image2 연결
+    cv2.imshow(title, concated)                                                     # 윈도우 화면 출력
 
-# 영상 합성
-one, two = 0.6, 0.4                                       # 곱샘 비율
-image3 = cv2.addWeighted(image1, one, image2, two, 0)     # 두영상 비율에 따른 더하기
+def onChange0(value):
+    proportion[0] = value * 0.01                                                    # image1 비율 값 % 단위 변경
+    desolve()                                                                       # desolve 함수 실행
 
-# 영상 3개가 들어갈 배열 생성하고 영상 넣기
-w, h = image1.shape                                          # image1의 가로, 세로 길이
-dst = np.zeros((w, h*3), np.uint8)                           # image1의 가로, 세로 * 3의 배열 생성
-dst[0:h, 0:w] = image1[0:h, 0:w]                             # 배열의 맨 앞에 image1 넣기
-dst[:, w*2:] = image2[0:h, 0:w]                              # 배열의 맨 뒤에 image2 넣기
-dst[0:h, w:w*2] = image3[0:h, 0:w]                           # 배열의 가운데에 image3 넣기
+def onChange1(value):
+    proportion[1] = value * 0.01                                                    # image2 비율 값 % 단위 변경
+    desolve()                                                                       # desolve 함수 실행
 
-title = ' dst'
-cv2.imshow(title, dst)  #윈도우에 영상 표시
-
-# one, two값을 조절할 트랙바 달기
-cv2.createTrackbar('image1', title, 50, 100, bar)
-cv2.createTrackbar('image2', title, 50, 100, bar)
-
-cv2.waitKey(0)
+cv2.namedWindow(title)                                                              # 윈도우 이름 재설정
+cv2.createTrackbar("image1", title, proportion[0], 100, onChange0)                  # 트랙바 생성(image1 비율과 연결)
+cv2.createTrackbar("image2", title, proportion[1], 100, onChange1)                  # 트랙바 생성(image2 비율과 연결)
+onChange0(proportion[0])                                                            # 비율값으로 변경
+onChange1(proportion[1])                                                            # 비율값으로 변경
+cv2.waitKey(0)                                                                      # 키 입력대기
+cv2.destroyAllWindows()                                                             # 모든 윈도우 소멸
